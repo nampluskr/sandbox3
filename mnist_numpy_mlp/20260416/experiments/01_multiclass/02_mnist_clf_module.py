b3d4e3dc63acb64e1dda241732316e56e4367a57
nampluskr <37031766+nampluskr@users.noreply.github.com>
2026-04-16 16:47:30 +0900

    Create 02_mnist_clf_module.py

---
+++ mnist_numpy_mlp/20260416/experiments/01_multiclass/02_mnist_clf_module.py
@@ -0,0 +1,130 @@
+import sys
+import os
+
+ROOT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..")
+CONFIG_DIR = os.path.normpath(os.path.join(ROOT_DIR, "configs"))
+SOURCE_DIR = os.path.normpath(os.path.join(ROOT_DIR, "src"))
+
+if SOURCE_DIR not in sys.path:
+    sys.path.insert(0, SOURCE_DIR)
+
+#################################################################
+## 1. Hyperparameters
+#################################################################
+import numpy as np
+
+from common.config import load_config
+from common.mnist import load_images, load_labels, one_hot
+from common.functions import softmax, cross_entropy, accuracy
+from common.modules import Linear, Sigmoid, Sequential
+from common.dataloader import Dataloader
+from training.optimizers import SGD
+
+config = load_config(CONFIG_DIR, "multiclass.yaml")
+
+SEED = config["seed"]
+DATASET_DIR = config["dataset_dir"]
+BATCH_SIZE = config["batch_size"]
+LEARNING_RATE = float(config["learning_rate"])
+NUM_EPOCHS = config["num_epochs"]
+NUM_SAMPLES = config["num_samples"]
+
+#################################################################
+## 2. Data loading and preprocessing
+#################################################################
+x_train = load_images(DATASET_DIR, "train")                     # (60000, 28, 28)
+y_train = load_labels(DATASET_DIR, "train")                     # (60000,)
+x_test = load_images(DATASET_DIR, "test")                       # (10000, 28, 28)
+y_test = load_labels(DATASET_DIR, "test")                       # (10000,)
+
+x_train = x_train.reshape(-1, 784).astype(np.float32) / 255.0   # (60000, 784)
+x_test = x_test.reshape(-1, 784).astype(np.float32) / 255.0     # (10000, 784)
+
+y_train = one_hot(y_train, num_classes=10).astype(np.float32)   # (60000, 10)
+y_test = one_hot(y_test, num_classes=10).astype(np.float32)     # (10000, 10)
+
+#################################################################
+## 3. Modeling
+#################################################################
+np.random.seed(SEED)
+
+model = Sequential(
+    Linear(784, 256),
+    Sigmoid(),
+    Linear(256, 128),
+    Sigmoid(),
+    Linear(128, 10),
+)
+optimizer = SGD(model, lr=LEARNING_RATE)
+
+#################################################################
+## 4. Training
+#################################################################
+print(f"\n>> Training:")
+
+train_loader = Dataloader(x_train, y_train, batch_size=BATCH_SIZE, shuffle=True)
+test_loader = Dataloader(x_test, y_test, batch_size=BATCH_SIZE, shuffle=False)
+
+for epoch in range(1, NUM_EPOCHS + 1):
+    total_loss = 0
+    total_acc = 0
+    total_size = 0
+
+    for x, y in train_loader:
+        batch_size = len(x)
+        total_size += batch_size
+
+        # Forward propagation
+        logits = model(x)
+        preds = softmax(logits)
+        loss = cross_entropy(preds, y)
+        acc = accuracy(preds, y)
+
+        # Backward propagation
+        dout = (preds - y) / batch_size
+        model.backward(dout)
+
+        # Update weights
+        optimizer.step()
+
+        total_loss += loss * batch_size
+        total_acc += acc * batch_size
+
+    print(f"[{epoch:>2}/{NUM_EPOCHS}] loss:{total_loss/total_size:.3f} acc:{total_acc/total_size:.3f}")
+
+#################################################################
+## 5. Evaluaton
+#################################################################
+print(f"\n>> Evaluation:")
+
+total_loss = 0.0
+total_acc = 0.0
+total_size = 0
+
+for x, y in test_loader:
+    batch_size = x.shape[0]
+    total_size += batch_size
+
+    logits = model(x)
+    preds = softmax(logits)
+    loss = cross_entropy(preds, y)
+    acc = accuracy(preds, y)
+
+    total_loss += loss * batch_size
+    total_acc += acc * batch_size
+
+print(f"loss:{total_loss/total_size:.3f} acc:{total_acc/total_size:.3f}")
+
+#################################################################
+## 6. Prediction
+#################################################################
+print(f"\n>> Prediction:")
+
+x = x_test[:NUM_SAMPLES]
+y = y_test[:NUM_SAMPLES]
+
+logits = model(x)
+preds = softmax(logits)
+
+for i in range(NUM_SAMPLES):
+    print(f"Target: {y[i].argmax()} | Prediction: {preds[i].argmax()}")

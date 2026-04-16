1f61e0b7852bad160f315ded89db8ea4248f87fb
nampluskr <37031766+nampluskr@users.noreply.github.com>
2026-04-16 16:54:43 +0900

    Create mnist.py

---
+++ mnist_numpy_mlp/20260416/src/common/mnist.py
@@ -0,0 +1,19 @@
+import os
+import gzip
+import numpy as np
+
+
+def load_images(data_dir, split="train"):
+    filename = "train-images-idx3-ubyte.gz" if split == "train" else "t10k-images-idx3-ubyte.gz"
+    filepath = os.path.join(data_dir, filename)
+    with gzip.open(filepath, 'rb') as f:
+        data = np.frombuffer(f.read(), np.uint8, offset=16)
+    return data.reshape(-1, 28, 28).copy()
+
+
+def load_labels(data_dir, split="train"):
+    filename = "train-labels-idx1-ubyte.gz" if split == "train" else "t10k-labels-idx1-ubyte.gz"
+    filepath = os.path.join(data_dir, filename)
+    with gzip.open(filepath, 'rb') as f:
+        data = np.frombuffer(f.read(), np.uint8, offset=8)
+    return data.copy()

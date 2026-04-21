3cf0ffcb92afb9e6f190efd5cac48639ab5c0cea
nampluskr <37031766+nampluskr@users.noreply.github.com>
2026-04-21 18:10:53 +0900

    Create trainer.py

---
+++ gan_basics_pytorch/20260421/src/training/trainer.py
@@ -0,0 +1,28 @@
+# src/training/trainer.py
+
+import os
+import sys
+from tqdm import tqdm
+
+
+def train(model, dataloader):
+    model.train()
+    metrics = {}
+    total_size = 0
+
+    with tqdm(dataloader, desc="Train", file=sys.stdout, leave=False, ascii=True) as progress_bar:
+        for batch in progress_bar:
+            train_results = model.train_step(batch)
+            total_size += train_results["batch_size"]
+
+            for name, value in train_results.items():
+                if name != "batch_size":
+                    metrics.setdefault(name, 0.0)
+                    metrics[name] += float(value) * train_results["batch_size"]
+
+            info = {name: f"{value / total_size:.3f}" for name, value in metrics.items()}
+            progress_bar.set_postfix(info)
+
+    results = {name: value / total_size for name, value in metrics.items()}
+    results["info"] = ", ".join([f"{name}:{value:.3f}" for name, value in results.items()])
+    return results

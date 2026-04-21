99e2b098587bcfcec5b551d77f21dc5d9e316771
nampluskr <37031766+nampluskr@users.noreply.github.com>
2026-04-21 18:09:48 +0900

    Create blocks.py

---
+++ gan_basics_pytorch/20260421/src/models/blocks.py
@@ -0,0 +1,30 @@
+# src/models/blocks.py
+
+import torch
+import torch.nn as nn
+
+
+class DeconvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.block = nn.Sequential(
+            nn.ConvTranspose2d(in_channels, out_channels, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(True),
+        )
+
+    def forward(self, x):
+        return self.block(x)
+
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.block = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(0.2, inplace=True),
+        )
+
+    def forward(self, x):
+        return self.block(x)

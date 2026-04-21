6a85b87ab889a4b3810e301ee68f2a9eec93fab2
nampluskr <37031766+nampluskr@users.noreply.github.com>
2026-04-21 18:59:09 +0900

    Update configs.py

--- gan_basics_pytorch/20260421/src/configs.py
+++ gan_basics_pytorch/20260421/src/configs.py
@@ -1,6 +1,6 @@
-import os
+# import os
 import yaml
-import torch
+# import torch
 
 
 def load_config(config_path):
@@ -8,9 +8,9 @@ def load_config(config_path):
         return yaml.safe_load(f)
 
 
-def get_device(config):
-    use_cuda = config['train'].get('device', 'cuda')
-    return torch.device('cuda' if use_cuda and torch.cuda.is_available() else 'cpu')
+# def get_device(config):
+#     use_cuda = config['train'].get('device', 'cuda')
+#     return torch.device('cuda' if use_cuda and torch.cuda.is_available() else 'cpu')
 
 
 def merge_configs(default, override):
@@ -23,10 +23,10 @@ def merge_configs(default, override):
     return merged
 
 
-def create_dirs(config):
-    dirs = [
-        config['paths']['output_dir'],
-        config['paths']['checkpoint_dir']
-    ]
-    for d in dirs:
-        os.makedirs(d, exist_ok=True)
+# def create_dirs(config):
+#     dirs = [
+#         config['paths']['output_dir'],
+#         config['paths']['checkpoint_dir']
+#     ]
+#     for d in dirs:
+#         os.makedirs(d, exist_ok=True)

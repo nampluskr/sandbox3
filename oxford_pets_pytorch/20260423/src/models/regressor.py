5e40ede783d2fe59cb93ec2afc7f44a9022b8616
nampluskr <37031766+nampluskr@users.noreply.github.com>
2026-04-23 16:48:12 +0900

    Update regressor.py

--- oxford_pets_pytorch/20260423/src/models/regressor.py
+++ oxford_pets_pytorch/20260423/src/models/regressor.py
@@ -5,6 +5,24 @@ import torch.optim as optim
 from torchmetrics.regression import MeanSquaredError, MeanAbsoluteError
 
 
+def batch_iou(boxes1, boxes2):
+    x1 = torch.max(boxes1[..., 0], boxes2[..., 0])
+    y1 = torch.max(boxes1[..., 1], boxes2[..., 1])
+    x2 = torch.min(boxes1[..., 2], boxes2[..., 2])
+    y2 = torch.min(boxes1[..., 3], boxes2[..., 3])
+
+    inter_w = torch.clamp(x2 - x1, min=0)
+    inter_h = torch.clamp(y2 - y1, min=0)
+    inter_area = inter_w * inter_h
+
+    area1 = (boxes1[..., 2] - boxes1[..., 0]) * (boxes1[..., 3] - boxes1[..., 1])
+    area2 = (boxes2[..., 2] - boxes2[..., 0]) * (boxes2[..., 3] - boxes2[..., 1])
+    union_area = area1 + area2 - inter_area
+
+    iou = inter_area / (union_area + 1e-8)
+    return iou
+
+
 class Regressor(nn.Module):
     def __init__(self, model, optimizer=None, device=None):
         super().__init__()
